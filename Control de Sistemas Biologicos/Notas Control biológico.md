2aadd9c1a9ae0483d85e36ae10f335af5d1f2666
Tomás Vidal <tomividal9@gmail.com>
2025-03-23 13:29:49 -0300

    simulacion de PHB

---
+++ Control de Sistemas Biologicos/Notas Control biológico.md
@@ -0,0 +1,3 @@
+# Preguntas
+- [ ] ¿Qué es exactamente D? ¿Por qué a veces aparece con diferentes valores para diferentes reactivos? ¿Cómo interpretar $S_{in}-S$?
+- [ ] Para mi caso de producción de bioplástico (II). ¿Se usan dos modelos para cada ritmo de reacción? ¿$r_{x}$ y $r_{p}$?
---
+++ Control de Sistemas Biologicos/TP1/HaldaneMonodModel.m
@@ -0,0 +1,16 @@
+function model = HaldaneMonodModel(s, n, params)
+% Es un modelo que está compuesto por el producto de dos:
+% El modelo de Haldane y el de Monod
+% Se espera que params sea una estructura que tenga todos los 
+% parámetros requeridos, siguiendo el siguiente esquema:
+%
+% model = max * (s/( k1 + s + s*s/k2 )) * (1/(k3 + n))
+
+max=params.max;
+k1=params.k1;
+k2=params.k2;
+k3=params.k3;
+
+model = max * (s/( k1 + s + s*s/k2 )) * (1/(k3 + n));
+
+end
