1c00bb5646cd1c5e1758b740748d0141c0048a26
Tomás Vidal <tomividal9@gmail.com>
2025-05-31 20:33:02 -0300

    El auto y el control se comunican con el modulo nRF24

---
+++ Taller de sistemas digitales/Software/Auto/include/definitions.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+// - - - - - - - - - - -  FUNCIONES - - - - - - - - - -
+
+/**
+ * Los motores se controlan variando una pwm.
+ * Se tienen que configurar los siguientes registros
+ * con un valor de 0 a 255.
+ * OCR0A = 128; // 50% duty cycle on motor 1
+ * OCR2B = 200; // ~78% duty cycle on motor 2
+ */
+void init_adc();
+void init_button_interrupt();
---
+++ Taller de sistemas digitales/Software/Control/README.md
@@ -0,0 +1,9 @@
+# Template
+
+## Changing uC?
+- Change it in: _.vscode/c_cpp_properties.json_ (linting/completion)
+- Change it in: _Makefile_ (compiler)
+
+## Possible problems
+- Linting not working properly
+Change the path of the _avr-gcc_ and _avr-libc_ in _c_cpp_properties.json_
