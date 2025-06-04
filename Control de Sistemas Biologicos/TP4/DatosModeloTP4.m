19c1eb4eaebb991235f29af1b6c9470779ffa0a5
Tomás Vidal <tomividal9@gmail.com>
2025-06-04 19:37:36 -0300

    CSB. TP4 terminado, solo falta preguntar

--- Control de Sistemas Biologicos/TP4/DatosModeloTP4.m
+++ Control de Sistemas Biologicos/TP4/DatosModeloTP4.m
@@ -5,6 +5,7 @@ x0=5;
 s0=0.1;
 mu_max=0.46;
 Ks=1.2;
+Kis=16.728;
 
 v0=0.1;
 states0 = [x0;s0]*1.5;
@@ -12,10 +13,12 @@ states0 = [x0;s0]*1.5;
 % Parámetros que van dentro del modelo del sistema
 modelParameters.K = [1;-ks];
 modelParameters.xi_in = [0;s_in];
+modelParameters.ks=ks;
 
 % Defino los parámetros de los modelos cinéticos
 modelParameters.mu_model.mu_max=mu_max;
 modelParameters.mu_model.Ks=Ks;
+modelParameters.mu_model.Kis=Kis;
 
 F=zeros(length(modelParameters.K),1);
 Q=zeros(length(modelParameters.K),1);
